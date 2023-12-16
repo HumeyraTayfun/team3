e019324ef6017030bf1ad24603dd85b4cd1b823f
İhsan <altnskihsan@gmail.com>
2023-12-16 15:51:46 +0300

    ihsan3.gün

--- src/ihsan.java
+++ src/ihsan.java
@@ -4,5 +4,6 @@ public class ihsan {
         System.out.println("2.gün");
         System.out.println("3.gün");
         System.out.println("afasdfsdf");
+        System.out.println("DEĞİŞİKLİK");
     }
 }
