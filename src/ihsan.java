fa23c15efd03ca5e4d35b902c39718c092081688
İhsan <altnskihsan@gmail.com>
2023-12-16 14:55:19 +0300

    ihsan1.gün

---
+++ src/ihsan.java
@@ -0,0 +1,5 @@
+public class ihsan {
+    public static void main(String[] args) {
+        System.out.println("İhsan");
+    }
+}
--- src/testcase.java
+++ src/testcase.java
@@ -1,8 +1,5 @@
 public class testcase {
-    git init
-    git add .
-    git commit -m "first commit"
-    git branch -M main
-    git remote add origin https://github.com/HumeyraTayfun/team3.git
-    git push -u origin main
+    public static void main(String[] args) {
+        System.out.println("TEST1");
+    }
 }
