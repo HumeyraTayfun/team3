535beb536c464fb5a2546a1526224f85119056e9
Hümeyra Tayfun <humeyratayfun@hotmail.com>
2023-12-16 14:46:08 +0300

    first commit

---
+++ src/testcase.java
@@ -0,0 +1,8 @@
+public class testcase {
+    git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/HumeyraTayfun/team3.git
+    git push -u origin main
+}
